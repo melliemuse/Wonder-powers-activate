a0abbe4032538dc18a32f49fa71a8f1d7189904d
Melody Stern <melliemuse@gmail.com>
2019-10-17 19:29:03 -0500

    updated files with completed exercise

--- src/scripts/powers.js
+++ src/scripts/powers.js
@@ -1,4 +1,6 @@
 const flightSection = document.querySelector('#flight')
+const mindReadingSection = document.querySelector('#mindreading')
+const xraySection = document.querySelector('#xray')
 
 // Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
 
@@ -10,20 +12,55 @@ const flightHandlerFunction = () => {
 
 // Add the correct string as the first argument to addEventListener()
 
-document.querySelector("#activate-flight").addEventListener("click",  flightHandlerFunction())
+document.querySelector("#activate-flight").addEventListener("click",  flightHandlerFunction)
 
 
-// Have your developer tools open. When you click the button, the following element...
-// <section id="flight" class="power disabled">
-// Should then look like this.
 
-// <section id="flight" class="power enabled">
 // Once you have that working, write two more JavaScript functions, and two more querySelector().addEventListener() statements for handling the click event on the other two buttons. Those event handlers should perform the same step of removing disabled, and adding enabled on the corresponding <section> elements
 
-// document.querySelector("#activate-mindreading").addEventListener()
-// document.querySelector("#activate-xray").addEventListener()
+const mindreadingHandlerFunction = () => {
+    mindReadingSection.classList.toggle('disabled')
+    mindReadingSection.className = 'enabled'
+}
+
+const xrayHandlerFunction = () => {
+    xraySection.classList.toggle('disabled')
+    xraySection.className = 'enabled'
+}
+
+
+document.querySelector("#activate-mindreading").addEventListener("click", mindreadingHandlerFunction)
+document.querySelector("#activate-xray").addEventListener("click", xrayHandlerFunction)
+
+
+
 // Once that is complete, add two more buttons
 
 // Enable All Powers
 // Disable All Powers
 // Write two more event handlers for activating and deactivating all powers when the corresponding buttons are clicked. You will need to use the document.querySelectorAll() method for these.
+
+const activateAllButton = document.querySelector('#activate-all')
+const deactivateAllButton = document.querySelector('#deactivate-all')
+const allSections = document.querySelectorAll('section')
+
+
+const activateAllFunction = () => {
+    allSections.forEach(function(section) {
+        section.classList.toggle('disabled')
+        section.className = 'enabled'
+    })
+}
+
+activateAllButton.addEventListener("click", activateAllFunction)
+
+const deactivateAllFunction = () => {
+    allSections.forEach(function(section) {
+        section.classList.toggle('enabled')
+        section.className = 'disabled'
+    })
+    
+}
+
+
+deactivateAllButton.addEventListener("click", deactivateAllFunction)
